aa4324b784fcf805476fe78e356fd62bbc25b952
ErnestoOrtegaDev <ernestoortega1423@gmail.com>
2026-06-22 11:27:40 -0600

    fix: Refactorizar nombres de archivos

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import userController from "../controllers/users.controller.js";
+import userController from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/", userController.createUserWithDefaultPassword);
 router.put("/:id", userController.updateUser);
-router.delete("/soft/:id", userController.softDeleteUser);
+router.delete("/delete/:id", userController.softDeleteUser);
 router.delete("/hard/:id", userController.hardDeleteUser);
 
 export default router;
